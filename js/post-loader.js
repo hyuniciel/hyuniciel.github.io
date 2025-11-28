b46a6668c58bbcdf3dab52df4c269d42d85f5c52
hyuniciel <hyuniciel@gmail.com>
2025-11-28 13:32:13 +0900

    ai교육

--- js/post-loader.js
+++ js/post-loader.js
@@ -213,9 +213,9 @@
     const script = document.createElement('script');
     script.src = 'https://giscus.app/client.js';
     script.setAttribute('data-repo', 'hyuniciel/hyuniciel.github.io');
-    script.setAttribute('data-repo-id', 'YOUR_REPO_ID'); // TODO: Replace with actual repo ID
+    script.setAttribute('data-repo-id', 'R_kgDOQec18Q'); // TODO: Replace with actual repo ID
     script.setAttribute('data-category', 'General');
-    script.setAttribute('data-category-id', 'YOUR_CATEGORY_ID'); // TODO: Replace with actual category ID
+    script.setAttribute('data-category-id', 'DIC_kwDOQec18c4CzIyg'); // TODO: Replace with actual category ID
     script.setAttribute('data-mapping', 'pathname');
     script.setAttribute('data-strict', '0');
     script.setAttribute('data-reactions-enabled', '1');
